ace1b434ab002505d80e8b6a631f4b5960b6984c
tarheelnc83@outlook.com <tarheelnc83@outlook.com>
2018-03-07 16:09:35 +0000

    automatically backed up by learn

--- hoisting.js
+++ hoisting.js
@@ -11,12 +11,12 @@ function callMe() {
 
 function crazy() {
   // fix the code in here:
-
-
-  var thisIsCrazy = function (){
+  var thisIsCrazy
+  thisIsCrazy();
+   thisIsCrazy = function (){
     console.log("hey!!!")
   }
-    thisIsCrazy();
+
 }
 
 function sayMyName() {
